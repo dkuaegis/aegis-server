a7dd3963a772bfef5f97f1b93ad96ccc1ad87897
Yun Seongmin <ilcm96@gmail.com>
2025-01-02 14:54:12 +0900

    feat: 임시 멤버 엔티티 추가 (#6)

---
+++ src/main/java/aegis/server/domain/member/domain/Member.java
@@ -0,0 +1,22 @@
+package aegis.server.domain.member.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+}
