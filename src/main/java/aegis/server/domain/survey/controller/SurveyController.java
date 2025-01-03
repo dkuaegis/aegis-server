449740d7235b6f2f50c971137994691355eb6550
harvey <alstjr971@naver.com>
2025-01-03 20:21:37 +0900

    feat: RestController 중복 URL을 RequestMapping으로 그룹화

--- src/main/java/aegis/server/domain/survey/controller/SurveyController.java
+++ src/main/java/aegis/server/domain/survey/controller/SurveyController.java
@@ -2,41 +2,38 @@ package aegis.server.domain.survey.controller;
 
 import aegis.server.domain.survey.domain.SurveyDto;
 import aegis.server.domain.survey.service.SurveyService;
+import aegis.server.global.security.annotation.LoginUser;
+import aegis.server.global.security.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/survey")
 public class SurveyController {
 
     private final SurveyService surveyService;
 
-    /**
-     * /api/survey/1
-     */
-    @GetMapping("/api/survey/{memberId}")
+    @GetMapping("/{memberId}")
     public ResponseEntity<SurveyDto> getSurvey(@PathVariable Long memberId) {
         SurveyDto surveyDto = surveyService.findByMemberId(memberId);
         return new ResponseEntity<>(surveyDto, HttpStatus.OK);
     }
 
-    /**
-     * {
-     *     "memberId": 1,
-     *     "interestFields": [
-     *         "SECURITY_WEBHACKING",
-     *         "WEB_FRONTEND"
-     *     ],
-     *     "registrationReason": "JUST DO IT",
-     *     "feedBack": "GOOD"
-     * }
-     */
-    @PostMapping("/api/survey")
-    public ResponseEntity<SurveyDto> survey(@Validated @RequestBody SurveyDto surveyDto) {
-        SurveyDto save = surveyService.save(surveyDto);
-        return new ResponseEntity<>(save, HttpStatus.CREATED);
+    @PostMapping
+    public ResponseEntity<SurveyDto> createSurvey(@Validated @RequestBody SurveyDto surveyDto, @LoginUser SessionUser sessionUser) {
+        surveyService.save(surveyDto, sessionUser);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<SurveyDto>> getAllSurveys() {
+        List<SurveyDto> allSurveys = surveyService.getAllSurveys();
+        return new ResponseEntity<>(allSurveys, HttpStatus.OK);
     }
 }
