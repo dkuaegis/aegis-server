dd30c4d7d18eae8e792e2e5dabaf0fdcc1a2e214
Yun Seongmin <ilcm96@gmail.com>
2025-01-05 19:47:49 +0900

    fix: 멤버 엔티티의 권한을 적용하지 않던 문제 해결 (#13)

--- src/main/java/aegis/server/global/security/oidc/CustomOidcUser.java
+++ src/main/java/aegis/server/global/security/oidc/CustomOidcUser.java
@@ -2,8 +2,11 @@ package aegis.server.global.security.oidc;
 
 import aegis.server.domain.member.domain.Member;
 import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+import java.util.Collections;
 
 @Getter
 public class CustomOidcUser extends DefaultOidcUser {
@@ -11,7 +14,11 @@ public class CustomOidcUser extends DefaultOidcUser {
     private final UserAuthInfo userAuthInfo;
 
     public CustomOidcUser(OidcUser oidcUser, Member member) {
-        super(oidcUser.getAuthorities(), oidcUser.getIdToken(), oidcUser.getUserInfo());
+        super(
+                Collections.singleton(new SimpleGrantedAuthority(member.getRole().getKey())),
+                oidcUser.getIdToken(),
+                oidcUser.getUserInfo()
+        );
         this.userAuthInfo = UserAuthInfo.from(member);
     }
 }
