452fd8de2f1e3eece9250d1cc94a4105c76eaa42
Coffeecaat <139877290+Coffeecaat@users.noreply.github.com>
2025-01-10 22:57:30 +0900

    feat: 에타 시간표 크롤링 API 컨트롤러 추가

---
+++ src/main/java/aegis/server/domain/timeTable/controller/timeTableController.java
@@ -0,0 +1,30 @@
+package timeTable.EverytimeCrawler.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import aegis.server.domain.timeTable.service.timeTableService;
+
+import java.util.List;
+
+@Controller
+public class timeTableController {
+
+    private final timeTableService timeTableService;
+
+    @Autowired
+    public timeTableController(timeTableService timeTableService){
+        this.timeTableService = timeTableService;
+    }
+
+    @GetMapping("/crawlTimetable")
+    public String crawlTimetable(Model model){
+        List<String> timetableData = timeTableService.crawlTimetable();
+
+        model.addAttribute("timetableData", timetableData);
+
+        return "timetable";
+    }
+
+}
