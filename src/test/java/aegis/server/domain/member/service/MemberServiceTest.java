70ce8d0f46230cf3fe5410959c10a5ebc12f2049
hoon4041 <simjaehoon3@naver.com>
2025-01-15 21:00:39 +0900

    getMember메소드 변경에 따른 테스트코드 변경

--- src/test/java/aegis/server/domain/member/service/MemberServiceTest.java
+++ src/test/java/aegis/server/domain/member/service/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package aegis.server.domain.member.service;
 
+import aegis.server.common.IntegrationTest;
 import aegis.server.domain.member.domain.*;
 import aegis.server.domain.member.dto.request.MemberUpdateRequest;
 import aegis.server.domain.member.dto.response.MemberResponse;
@@ -57,7 +58,7 @@ public class MemberServiceTest {
             when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
 
             // when
-            MemberResponse memberResponse = memberService.getMember(sessionUser, member.getId());
+            MemberResponse memberResponse = memberService.getMember(sessionUser);
 
             // then
             assertEquals(member.getId(), memberResponse.getId());
@@ -74,7 +75,7 @@ public class MemberServiceTest {
 
             // when & then
             assertThrows(IllegalArgumentException.class, () ->
-                    memberService.getMember(sessionUser, invalidMemberId));
+                    memberService.getMember(sessionUser));
         }
     }
 
