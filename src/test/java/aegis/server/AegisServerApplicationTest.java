dfda0de8209867805d185fd23a85d89e7265f622
Yun Seongmin <ilcm96@gmail.com>
2025-01-27 23:14:22 +0900

    refactor: 프로젝트 재작성을 위한 초기 상태 복원

---
+++ src/test/java/aegis/server/AegisServerApplicationTest.java
@@ -0,0 +1,13 @@
+package aegis.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
