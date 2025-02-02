d1696455d2506f7a7ceceb04e01038bc34a23913
Yun Seongmin <ilcm96@gmail.com>
2025-02-03 01:28:38 +0900

    fix: CouponCreateRequest에서 discountAmount가 null 이면 NPE가 발생하던 문제 해결

--- src/main/java/aegis/server/domain/coupon/dto/request/CouponCreateRequest.java
+++ src/main/java/aegis/server/domain/coupon/dto/request/CouponCreateRequest.java
@@ -1,12 +1,13 @@
 package aegis.server.domain.coupon.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record CouponCreateRequest(
         @NotBlank String couponName,
-        @Positive BigDecimal discountAmount
+        @NotNull @Positive BigDecimal discountAmount
 ) {
 }
