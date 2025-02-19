63690acdec747e0c2d2a5bbfd380ed75dd453da1
Yun Seongmin <ilcm96@gmail.com>
2025-02-19 18:23:00 +0900

    refactor: 디스코드 인증 코드가 Redis에 존재하지 않을 때만 새로 생성 (#56)

--- src/main/java/aegis/server/domain/discord/repository/DiscordVerificationRepository.java
+++ src/main/java/aegis/server/domain/discord/repository/DiscordVerificationRepository.java
@@ -4,8 +4,11 @@ import aegis.server.domain.discord.domain.DiscordVerification;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DiscordVerificationRepository extends CrudRepository<DiscordVerification, String> {
 
     List<DiscordVerification> findAll();
+
+    Optional<DiscordVerification> findByMemberId(Long memberId);
 }
