2287c070a4ccd9b106ee713711b374caad5269ff
Yun Seongmin <ilcm96@gmail.com>
2025-03-05 13:02:25 +0900

    refactor: repository에서 importer에서 사용하는 메서드 유지

--- src/main/java/aegis/server/domain/payment/repository/PaymentRepository.java
+++ src/main/java/aegis/server/domain/payment/repository/PaymentRepository.java
@@ -3,8 +3,10 @@ package aegis.server.domain.payment.repository;
 import aegis.server.domain.common.domain.YearSemester;
 import aegis.server.domain.member.domain.Student;
 import aegis.server.domain.payment.domain.Payment;
+import aegis.server.domain.payment.domain.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 import static aegis.server.global.constant.Constant.CURRENT_YEAR_SEMESTER;
@@ -22,4 +24,6 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     default Optional<Payment> findByExpectedDepositorNameInCurrentYearSemester(String expectedDepositorName) {
         return findByExpectedDepositorNameAndYearSemester(expectedDepositorName, CURRENT_YEAR_SEMESTER);
     }
+
+    List<Payment> findAllByStatusAndYearSemester(PaymentStatus paymentStatus, YearSemester currentYearSemester);
 }
