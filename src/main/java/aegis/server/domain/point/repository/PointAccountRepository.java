df9d89fc040809df82a20f1c841440f52884b656
Yun Seongmin <ilcm96@gmail.com>
2025-07-18 02:49:24 +0900

    feat: 로그인 시 자동으로 포인트 계좌를 생성

--- src/main/java/aegis/server/domain/point/repository/PointAccountRepository.java
+++ src/main/java/aegis/server/domain/point/repository/PointAccountRepository.java
@@ -2,6 +2,9 @@ package aegis.server.domain.point.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import aegis.server.domain.member.domain.Member;
 import aegis.server.domain.point.domain.PointAccount;
 
-public interface PointAccountRepository extends JpaRepository<PointAccount, Long> {}
+public interface PointAccountRepository extends JpaRepository<PointAccount, Long> {
+    boolean existsByMember(Member member);
+}
