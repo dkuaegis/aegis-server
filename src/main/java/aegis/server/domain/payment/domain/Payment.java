0764aed818d368f1c812d15bc4c71a33a70b9ff4
Yun Seongmin <ilcm96@gmail.com>
2025-07-17 15:09:20 +0900

    refactor: 여러 학기 지원하도록 연관관계 변경

--- src/main/java/aegis/server/domain/payment/domain/Payment.java
+++ src/main/java/aegis/server/domain/payment/domain/Payment.java
@@ -27,7 +27,7 @@ public class Payment extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
