9082bd5c922e73170bc3ce2ca11d4e37b393cffa
Yun Seongmin <ilcm96@gmail.com>
2025-07-17 15:08:30 +0900

    refactor: 2025년 2학기로 전환

--- src/main/java/aegis/server/domain/common/domain/YearSemester.java
+++ src/main/java/aegis/server/domain/common/domain/YearSemester.java
@@ -3,10 +3,13 @@ package aegis.server.domain.common.domain;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.Year;
+
 @Getter
 @RequiredArgsConstructor
 public enum YearSemester {
-    YEAR_SEMESTER_2025_1("2025-1");
+    YEAR_SEMESTER_2025_1("2025-1"),
+    YEAR_SEMESTER_2025_2("2025-2");
 
     private final String value;
 }
