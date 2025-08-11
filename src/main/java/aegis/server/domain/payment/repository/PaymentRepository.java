45783060581344789695d9847063e0ca9400b8ee
Yun Seongmin <ilcm96@gmail.com>
2025-08-12 01:22:52 +0900

    fix: 본인의 결제를 가져오는 과정에서 발생하는 N+1 문제 해결

--- src/main/java/aegis/server/domain/payment/repository/PaymentRepository.java
+++ src/main/java/aegis/server/domain/payment/repository/PaymentRepository.java
@@ -58,6 +58,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     List<Payment> findAllByStatusAndYearSemester(PaymentStatus paymentStatus, YearSemester currentYearSemester);
 
-    @Query("SELECT p FROM Payment p WHERE p.member.id = :memberId ORDER BY p.yearSemester DESC, p.createdAt DESC")
+    @Query(
+            "SELECT p FROM Payment p JOIN FETCH p.member WHERE p.member.id = :memberId ORDER BY p.yearSemester DESC, p.createdAt DESC")
     List<Payment> findAllByMemberIdOrderByYearSemesterDescCreatedAtDesc(Long memberId);
 }
