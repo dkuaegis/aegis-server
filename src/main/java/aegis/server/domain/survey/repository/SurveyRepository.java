455b340b552996749e68e2dffac05bc8b1d4cdf2
Yun Seongmin <ilcm96@gmail.com>
2025-08-16 17:28:46 +0900

    refactor(BE-210): 설문조사가 한 학기에 대해서만 생성될 수 있던 설계 수정 (#100)

--- src/main/java/aegis/server/domain/survey/repository/SurveyRepository.java
+++ src/main/java/aegis/server/domain/survey/repository/SurveyRepository.java
@@ -1,15 +1,21 @@
 package aegis.server.domain.survey.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import aegis.server.domain.member.domain.Member;
+import aegis.server.domain.common.domain.YearSemester;
 import aegis.server.domain.survey.domain.Survey;
 
-public interface SurveyRepository extends JpaRepository<Survey, Long> {
-    Optional<Survey> findByMember(Member member);
+import static aegis.server.global.constant.Constant.CURRENT_YEAR_SEMESTER;
 
-    List<Survey> findByMemberIn(List<Member> members);
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+
+    @Query("select s from Survey s where s.member.id = :memberId and s.yearSemester = :yearSemester")
+    Optional<Survey> findByMemberIdAndYearSemester(Long memberId, YearSemester yearSemester);
+
+    default Optional<Survey> findByMemberIdInCurrentYearSemester(Long memberId) {
+        return findByMemberIdAndYearSemester(memberId, CURRENT_YEAR_SEMESTER);
+    }
 }
