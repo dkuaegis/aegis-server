d8f259bd63498a3c1ddea7495bbf9280c5ae5abe
Yun Seongmin <ilcm96@gmail.com>
2025-09-30 19:22:27 +0900

    style: 인덴트 수정

--- src/main/java/aegis/server/global/config/RedisSessionSafetyConfig.java
+++ src/main/java/aegis/server/global/config/RedisSessionSafetyConfig.java
@@ -1,15 +1,15 @@
- package aegis.server.global.config;
+package aegis.server.global.config;
 
- import org.springframework.context.annotation.Bean;
- import org.springframework.context.annotation.Configuration;
- import org.springframework.context.annotation.Profile;
- import org.springframework.session.config.SessionRepositoryCustomizer;
- import org.springframework.session.data.redis.RedisIndexedSessionRepository;
- import org.springframework.session.data.redis.RedisSessionMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.session.config.SessionRepositoryCustomizer;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
+import org.springframework.session.data.redis.RedisSessionMapper;
 
- @Profile("!test")
- @Configuration
- public class RedisSessionSafetyConfig {
+@Profile("!test")
+@Configuration
+public class RedisSessionSafetyConfig {
 
     @Bean
     public SessionRepositoryCustomizer<RedisIndexedSessionRepository> safeRedisSessionMapper() {
@@ -24,4 +24,4 @@
             });
         };
     }
- }
+}
